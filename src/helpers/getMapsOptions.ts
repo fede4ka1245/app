deb0ec09a978ffab4c0f348175ad36e13f758eac
fedechka1245 <bragin.fedor96@gmail.com>
2022-10-31 19:22:33 +0500

    fix getMapsOptions

--- src/helpers/getMapsOptions.ts
+++ src/helpers/getMapsOptions.ts
@@ -28,11 +28,6 @@ export const getMapsOptions = (): Array<MapOption> => {
       mapSections: []
     },
     {
-      value: 'D-6',
-      label: 'D-6',
-      mapSections: []
-    },
-    {
       value: 'D-7',
       label: 'D-7',
       mapSections: []
@@ -53,11 +48,6 @@ export const getMapsOptions = (): Array<MapOption> => {
       mapSections: []
     },
     {
-      value: 'D-11',
-      label: 'D-11',
-      mapSections: []
-    },
-    {
       value: 'D-12',
       label: 'D-12',
       mapSections: []
@@ -68,53 +58,13 @@ export const getMapsOptions = (): Array<MapOption> => {
       mapSections: []
     },
     {
-      value: 'D-20',
-      label: 'D-20',
-      mapSections: []
-    },
-    {
       value: 'D-24',
       label: 'D-24',
       mapSections: []
     },
     {
-      value: 'D-27',
-      label: 'D-27',
-      mapSections: []
-    },
-    {
-      value: 'D-30',
-      label: 'D-30',
-      mapSections: []
-    },
-    {
-      value: 'D-40',
-      label: 'D-40',
-      mapSections: []
-    },
-    {
-      value: 'D-45',
-      label: 'D-45',
-      mapSections: []
-    },
-    {
-      value: 'D-60',
-      label: 'D-60',
-      mapSections: []
-    },
-    {
       value: 'D-81',
       label: 'D-81',
-      mapSections: []
-    },
-    {
-      value: 'D-108',
-      label: 'D-108',
-      mapSections: []
-    },
-    {
-      value: 'D-150',
-      label: 'D-150',
       mapSections: []
     }
   ];
