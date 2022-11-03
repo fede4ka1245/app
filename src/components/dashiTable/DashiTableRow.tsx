c455760fbe7e7f4ebe860e2f0a53f9a483170663
fedechka1245 <bragin.fedor96@gmail.com>
2022-11-04 01:37:27 +0500

    update dashi table

--- src/components/dashiTable/DashiTableRow.tsx
+++ src/components/dashiTable/DashiTableRow.tsx
@@ -36,12 +36,6 @@ const DashiTableRow = ({ row }: PlanetsTableRowProps) => {
     setIsOpen(!isOpen);
   }, [isOpen]);
 
-  useEffect(() => {
-    if (!row.planets.length) {
-      setIsOpen(true);
-    }
-  }, [row]);
-
   const planetsRowItem = useMemo(() => {
     return row.planets?.length ? `${row.planets.join(' / ')} / ${row.planet}` : row.planet;
   }, [row]);
@@ -58,34 +52,46 @@ const DashiTableRow = ({ row }: PlanetsTableRowProps) => {
     return getIsCurrentDates(row.dateStart, row.dateEnd);
   }, [row]);
 
+  const isMainRow = useMemo(() => {
+    return !row.planets.length;
+  }, [row]);
+
+  useEffect(() => {
+    if (isMainRow && isCurrentDates) {
+      setIsOpen(true);
+    }
+  }, [isMainRow, isCurrentDates]);
+
   return (
     <>
-      <Grid container className={classNames(styles.row, { [styles.isOpen]: isOpen, [styles.isOpened]: !!row.planets, [styles.isCurrent]: isCurrentDates })}>
-        <Grid item display={'flex'} alignItems={'center'} justifyContent={'center'}>
-          {!!row.subTable?.length && <IconButton onClick={toggleIsOpen}>
-            {!isOpen && <svg width="14" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1 1.5L5.24095 6.68338C5.39164 6.86756 5.39164 7.13244 5.24095 7.31662L1 12.5" stroke="#C3C9CD" strokeWidth="2" strokeLinecap="round"/>
-            </svg>}
-            {isOpen && <svg width="14" height="14" viewBox="0 0 13 8" fill="white" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 1.75L6.81662 5.99095C6.63244 6.14164 6.36756 6.14164 6.18338 5.99095L1 1.75" stroke="#37366B" strokeWidth="2" strokeLinecap="round"/>
-            </svg>}
-          </IconButton>}
+      <Grid className={styles.row}>
+        <Grid container className={classNames({ [styles.isOpen]: isOpen, [styles.isMain]: isMainRow, [styles.isCurrent]: isCurrentDates })}>
+          <Grid item display={'flex'} alignItems={'center'} justifyContent={'center'}>
+            {!!row.subTable?.length && <IconButton onClick={toggleIsOpen}>
+              {!isOpen && <svg width="14" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 1.5L5.24095 6.68338C5.39164 6.86756 5.39164 7.13244 5.24095 7.31662L1 12.5" stroke="#C3C9CD" strokeWidth="2" strokeLinecap="round"/>
+              </svg>}
+              {isOpen && <svg width="14" height="14" viewBox="0 0 13 8" fill="white" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 1.75L6.81662 5.99095C6.63244 6.14164 6.36756 6.14164 6.18338 5.99095L1 1.75" stroke="#37366B" strokeWidth="2" strokeLinecap="round"/>
+              </svg>}
+            </IconButton>}
+          </Grid>
+          <Grid item>
+            { planetsRowItem }
+          </Grid>
         </Grid>
-        <Grid item>
-          { planetsRowItem }
+        <Grid className={classNames({ [styles.isOpen]: isOpen, [styles.isMain]: isMainRow, [styles.isCurrent]: isCurrentDates })}>
+          {formattedDateStart}
+        </Grid>
+        <Grid className={classNames({ [styles.isOpen]: isOpen, [styles.isMain]: isMainRow, [styles.isCurrent]: isCurrentDates })}>
+          {formattedDateEnd}
         </Grid>
       </Grid>
-      <Grid className={classNames(styles.row, { [styles.isOpen]: isOpen, [styles.isOpened]: !!row.planets, [styles.isCurrent]: isCurrentDates })}>
-        {formattedDateStart}
-      </Grid>
-      <Grid className={classNames(styles.row, { [styles.isOpen]: isOpen, [styles.isOpened]: !!row.planets, [styles.isCurrent]: isCurrentDates })}>
-        {formattedDateEnd}
-      </Grid>
-      {isOpen && <>
+      {isOpen && <div className={classNames(styles.container, { [styles.outlined]: isMainRow })}>
         {row.subTable?.map((row, index) => (
           <DashiTableRow row={row} key={index} />
         ))}
-      </>}
+      </div>}
     </>
   );
 };
