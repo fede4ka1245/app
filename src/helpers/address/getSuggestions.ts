b0f30928716d00c523357254904e2ee5beefcc63
fedechka1245 <bragin.fedor96@gmail.com>
2022-11-04 20:39:00 +0500

    add location detect to proxy

--- src/helpers/address/getSuggestions.ts
+++ src/helpers/address/getSuggestions.ts
@@ -1,7 +1,7 @@
 import { formatSuggestions } from './formatSuggestions';
-import { Http } from '@capacitor-community/http';
 import { getGreenwichFromSign } from '../getGreenwichFromSign';
 import { TimeZoneData } from '../../models/types/TimeZoneData';
+import axios from 'axios';
 
 const url = 'https://j108.ru/atlas';
 const token = 'e2225e00a5ab3e3fd2e2855b86e2a1bb%241';
@@ -11,18 +11,21 @@ export const getSuggestions = (query: string, count = 15) => {
     url: `${url}/location?token=${token}&q=${query}&lang=ru`
   };
 
-  return Http.get(options)
-    .then(response => {
-      return formatSuggestions(JSON.parse(response.data)?.result).slice(0, count);
+  return axios.post('https://backm.alpha-astro.ru/horoscope/get-proxy/', options)
+    .then(({ data }) => {
+      return formatSuggestions(data.data.result).slice(0, count);
     });
 };
 
 export const getTimeZoneOffset = async (query: string, date: string, time: string): Promise<TimeZoneData> => {
   const options = {
-    url: `${url}/timezone?token=${token}&q=${query}&dt=${date} ${time}&lang=ru`
+    url: `${url}/timezone?token=${token}&q=${query}&dt=${date}%20${time}&lang=ru`
   };
 
-  const response = JSON.parse((await Http.get(options)).data).result[0];
+  const response = await axios.post('https://backm.alpha-astro.ru/horoscope/get-proxy/', options)
+    .then(({ data }) => {
+      return data.data.result[0];
+    });
 
   return {
     greenwich: getGreenwichFromSign(response?.z) as string,
