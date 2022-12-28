02b7d0b6202d7a552ffa73c3fbad6cb2b009762d
Nariman <onlnaovnar@gmail.com>
2022-12-29 00:18:07 +0600

    fix: changed the layout of mini courses in the general list of courses

--- src/pages/courses/Courses/components/MiniCourse/index.tsx
+++ src/pages/courses/Courses/components/MiniCourse/index.tsx
@@ -45,14 +45,16 @@ const MiniCourse: FC = () => {
           <div className={styles.price}>
             2590₽
           </div>
-          <div className={styles.more}>
-            Подробнее
+          <div className={styles.footer_right}>
+            <div className={styles.more}>
+              Подробнее
+            </div>
+            <button className={styles.button} onClick={navigateToMiniCourse}>
+              <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.5 12.5L5.74095 7.31662C5.89164 7.13244 5.89164 6.86756 5.74095 6.68338L1.5 1.5" stroke="#C3C9CD" strokeWidth="2" strokeLinecap="round"/>
+              </svg>
+            </button>
           </div>
-          <button className={styles.button} onClick={navigateToMiniCourse}>
-            <svg width="7" height="14" viewBox="0 0 7 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1.5 12.5L5.74095 7.31662C5.89164 7.13244 5.89164 6.86756 5.74095 6.68338L1.5 1.5" stroke="#C3C9CD" strokeWidth="2" strokeLinecap="round"/>
-            </svg>
-          </button>
         </div>
       </div>
     </div>
